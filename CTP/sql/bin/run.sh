9c90a369a09a95deb7e1eb5ed5f378b35a3129fa
Ying Li <neyeng@navercorp.com>
2016-06-06 18:24:07 +0800

    [CUBRIDQA-26] add the missing copyright statement
    add the missing copyright statement

--- CTP/sql/bin/run.sh
+++ CTP/sql/bin/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # 
+#Copyright (c) 2016, Search Solution Corporation? All rights reserved.
+#
 #Redistribution and use in source and binary forms, with or without 
 #modification, are permitted provided that the following conditions are met:
 #
