7a13ac0c7ab537f7ce14a45254ddbb32ce6a04f6
fanzaiqiang <fanzq@navercorp.com>
2016-07-28 14:09:49 +0800

    [CUBRIDQA-40] support general local test framework

---
+++ CTP/shell/src/com/navercorp/cubridqa/shell/main/GeneralLocalTest.java
@@ -0,0 +1,151 @@
+package com.navercorp.cubridqa.shell.main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.navercorp.cubridqa.common.LocalInvoker;
+import com.navercorp.cubridqa.shell.common.Constants;
+import com.navercorp.cubridqa.shell.result.FeedbackDB;
+import com.navercorp.cubridqa.shell.result.FeedbackFile;
+
+public class GeneralLocalTest {
+
+	private Context context;
+
+	public GeneralLocalTest(Context context) {
+		this.context = context;
+		this.context.setLogDir(context.getProperty("main.testing.category", "general"));
+		String feedbackType = context.getProperty("main.feedback.type", "").trim();
+		if (feedbackType.equalsIgnoreCase("file")) {
+			context.setFeedback(new FeedbackFile(context));
+		} else if (feedbackType.equalsIgnoreCase("database")) {
+			context.setFeedback(new FeedbackDB(context));
+		}
+		context.setTestBuild(context.getProperty("main.testing.build_id"));
+		context.setVersion(context.getProperty("main.testing.bits"));
+	}
+
+	public void start() {
+		System.out.println("=> Init Step: ");
+		System.out.println(invoke("init"));
+
+		System.out.println();
+
+		ArrayList<String> testCaseList = getTestCaseList();
+		if (testCaseList == null || testCaseList.size() == 0) {
+			System.out.println("[ERROR] Not found any test cases.");
+			return;
+		}
+
+		context.getFeedback().onTaskStartEvent(context.getTestBuild());
+		context.getFeedback().setTotalTestCase(testCaseList.size(), 0, 0);
+		Result result;
+		String testCaseName;
+		boolean isSucc;
+
+		System.out.println("=> Execute Step: ");
+		for (int i = 0; i < testCaseList.size(); i++) {
+			testCaseName = testCaseList.get(i);
+
+			context.getFeedback().onTestCaseStartEvent(testCaseName, "local");
+			System.out.print("[TESTCASE-" + (i + 1) + "] " + testCaseName);
+			result = invoke("execute " + testCaseName + " 2>&1 ", "IS_SUCC");
+			isSucc = com.navercorp.cubridqa.common.CommonUtils.convertBoolean(result.getProperty("IS_SUCC"));
+			System.out.println(isSucc ? " [SUCC]" : " [FAIL]");
+			context.getFeedback().onTestCaseStopEvent(testCaseName, isSucc, 0, result.getOutput(), "local", false, false, Constants.SKIP_TYPE_NO, 0);
+		}
+
+		System.out.println("=> Finish Step: ");
+		System.out.println(invoke("finish"));
+
+		System.out.println();
+		context.getFeedback().onTaskStopEvent();
+	}
+
+	private ArrayList<String> getTestCaseList() {
+		System.out.println("=> List Step: ");
+		Result output = invoke("list");
+		System.out.println(output);
+		System.out.println();
+		String[] arr = output.getOutput().split("\n");
+		ArrayList<String> list = new ArrayList<String>();
+		for (String i : arr) {
+			list.add(i);
+		}
+		return list;
+	}
+
+	private Result invoke(String scripts) {
+		return invoke(scripts, (String[]) null);
+	}
+
+	private Result invoke(String scripts, String... keys) {
+		if (scripts == null)
+			return null;
+
+		scripts = "cd ${CTP_HOME}; source shell/local/" + context.getProperty("main.testing.type") + ".sh; " + scripts;
+		if (keys != null && keys.length > 0) {
+			scripts = scripts + "; echo GPROPSTART\n";
+			for (String k : keys) {
+				scripts = scripts + "echo G_PROPERTY_" + k + "=${" + k + "}EEOOKK\n";
+			}
+		}
+		String result = LocalInvoker.exec(scripts, LocalInvoker.SHELL_TYPE_LINUX, false);
+
+		String output;
+		int pos = result.indexOf("GPROPSTART");
+		if (pos == -1) {
+			output = result;
+		} else {
+			output = result.substring(0, pos);
+		}
+
+		Properties props = null;
+		if (keys != null && keys.length > 0) {
+			props = new Properties();
+			String rKey, rValue;
+			int p1, p2;
+			for (String k : keys) {
+				rKey = "G_PROPERTY_" + k + "=";
+				p1 = result.indexOf(rKey);
+				if (p1 == -1)
+					continue;
+				p2 = result.indexOf("EEOOKK", p1);
+				rValue = result.substring(p1 + rKey.length(), p2);
+				props.setProperty(k, rValue.trim());
+			}
+		}
+		return new Result(output, props);
+	}
+
+	class Result {
+		String output;
+		Properties props;
+
+		public Result(String output, Properties props) {
+			this.output = output;
+			this.props = props;
+		}
+
+		public String getOutput() {
+			return this.output;
+		}
+
+		public String getProperty(String key) {
+			if (props == null)
+				return null;
+			return props.getProperty(key);
+		}
+
+		public String toString() {
+			return this.output;
+		}
+	}
+
+	public static void exec(String configFilename) throws IOException {
+		Context context = new Context(configFilename);
+		GeneralLocalTest test = new GeneralLocalTest(context);
+		test.start();
+	}
+}
