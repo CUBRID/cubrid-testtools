9c4e736754ab8ac3e5159253f9e02a278548701a
Zaiqiang Fan <fanzq@navercorp.com>
2016-08-03 17:16:52 +0800

    [CUBRIDQA-40] remove duplicated parameters in config file (#158)
    * [CUBRIDQA-40] enable to restart shell service
    
    * [CUBRIDQA-40] remove duplicated parameters in config file
    
    * [CUBRIDQA-40] remove duplicated parameter in general local test

--- CTP/isolation/src/com/navercorp/cubridqa/isolation/impl/FeedbackDB.java
+++ CTP/isolation/src/com/navercorp/cubridqa/isolation/impl/FeedbackDB.java
@@ -69,7 +69,7 @@ public class FeedbackDB implements Feedback {
 		Timestamp d = new Timestamp(System.currentTimeMillis());
 
 		String category = context.getProperty("main.testing.category");
-		String os = context.getProperty("main.testing.os");
+		String os = context.getProperty("main.testing.platform");
 
 		sql = "insert into shell_main(test_build, category, start_time, os, version) values(?, ?, ?, ?, ?)";
 
