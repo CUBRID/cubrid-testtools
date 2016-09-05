f5f4fd1589d4921f028bef012a3aeb7f342aaf5d
Zaiqiang Fan <fanzq@navercorp.com>
2016-09-05 14:06:48 +0800

    [CUBRIDQA-59] remove multi-nodes limitation for isolation (#201)
    * [CUBRIDQA-59] support local test for isolation test
    
    * [CUBRIDQA-59] change from LOCAL to local for instance

--- CTP/isolation/src/com/navercorp/cubridqa/isolation/Main.java
+++ CTP/isolation/src/com/navercorp/cubridqa/isolation/Main.java
@@ -58,13 +58,9 @@ public class Main {
 			context.setBuildBits(CommonUtils.getBuildBits(context
 					.getCubridPackageUrl()));
 			context.setReInstallTestBuildYn(true);
-		}else{
+		} else {
 			String envId = context.getEnvList().get(0);
-			String host = context.getInstanceProperty(envId, "ssh.host");
-			String port = context.getInstanceProperty(envId, "ssh.port");
-			String user = context.getInstanceProperty(envId, "ssh.user");
-			String pwd = context.getInstanceProperty(envId, "ssh.pwd");
-			SSHConnect ssh = new SSHConnect(host, user, port, pwd, "ssh"); 
+			SSHConnect ssh = IsolationHelper.createTestNodeConnect(context, envId);
 			String buildInfo = com.navercorp.cubridqa.shell.common.CommonUtils.getBuildVersionInfo(ssh);
 			context.setBuildId(CommonUtils.getBuildId(buildInfo));
 			context.setBuildBits(CommonUtils.getBuildBits(buildInfo));
