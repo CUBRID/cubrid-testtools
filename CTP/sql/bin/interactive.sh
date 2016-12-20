55f801863c0739e47697068e46db03fd94c05f36
cubridqa <neyeng@navercorp.com>
2016-12-20 18:50:44 +0800

    [CUBRIDQA-80] to make the interactive feature supports sql_by_cci

--- CTP/sql/bin/interactive.sh
+++ CTP/sql/bin/interactive.sh
@@ -22,7 +22,7 @@
 # SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-
+set -x
 function help()
 {
    echo "
@@ -30,13 +30,16 @@ function help()
 Usage: 
     help         print the usage of interactive
     run <arg>    the path of case file to run
+    run_cci <arg>    the path of case file to run_cci by cci driver
     quit         quit interactive mode "
    echo "
 For example:
     run .                                                 #run the current directory cases
     run ./_001_primary_key/_002_uniq                      #run the cases which are based on the relative path
     run test.sql                                          #run the test.sql file
-    run /home/user1/dailyqa/trunk/scenario/sql/_02_object #run the cases which are based on the absolute path"
+    run /home/user1/dailyqa/trunk/scenario/sql/_02_object #run the cases which are based on the absolute path
+    run_cci ./_001_primary_key/_002_uniq                  #run the cases which are based on the relative path
+    run_cci ./_001_primary_key/_002_uniq/test.sql         #run the cases file"
    echo ""
 }
 
@@ -61,20 +64,43 @@ function run()
    print_summary $log_file_in_interactive
 }
 
+function run_cci()
+{
+   case_file=`readlink -f $1`
+   port=`cat $CUBRID/conf/cubrid_broker.conf| grep "^SERVICE\|^BROKER_PORT" |grep -A1 "ON" | grep BROKER_PORT|tail -n 1|awk -F '=' '{print $NF}'|tr -d '[[:space:]]'`
+   result_folder="schedule_cdriver_`uname`_${scenario_alias_in_interactive}_`date +"%Y%m%d%H%M%S"`_${bits_in_interactive}"
+   cd $CTP_HOME/sql_by_cci
+   sh compile.sh >/dev/null 2>&1
+   cd -
+   $CTP_HOME/sql_by_cci/ccqt $port ${db_name_in_interactive} $scenario_alias_in_interactive $result_folder $case_file $CTP_HOME 2>&1 | tee $log_file_in_interactive
+
+   print_summary $log_file_in_interactive cci
+}
+
 
 function print_summary()
 {
    log_filename=$1
+   driver_type=$2
    
    resultDirTemp=`cat ${log_filename}|grep "^Result Root Dir"|head -n 1`
    resultDir=${resultDirTemp#*:}
-   resultSummaryInfoFile=${resultDir}/main.info
-   [ ! -f $resultSummaryInfoFile ] && echo "No Results!! please confirm your scenario path include valid case script(log:$log_filename)" && return
+   if [ -z "$driver_type" ];then
+   	resultSummaryInfoFile=${resultDir}/main.info
+   	[ ! -f $resultSummaryInfoFile ] && echo "No Results!! please confirm your scenario path include valid case script(log:$log_filename)" && return
+	failNum=`cat $resultSummaryInfoFile|grep 'fail:'|awk -F ':' '{print $2}'`
+        succNum=`cat $resultSummaryInfoFile|grep 'success:'|awk -F ':' '{print $2}'`
+        totalNum=`cat $resultSummaryInfoFile|grep 'total:'|awk -F ':' '{print $2}'`
+        elapseTime=`cat $resultSummaryInfoFile|grep 'totalTime:'|awk -F ':' '{print $2}'`
+   else
+	resultSummaryInfoFile=${resultDir}/summary.info
+   	[ ! -f $resultSummaryInfoFile ] && echo "No Results!! please confirm your scenario path include valid case script(log:$log_filename)" && return
+	failNum=`cat $resultSummaryInfoFile|grep "NOK"|wc -l`
+        totalNum=`cat ${log_filename}|grep "TOTAL_COUNT"|awk -F ':' '{print $2}'|tr -d ' '`
+        elapseTime=`cat ${log_filename}|grep "TOTAL_ELAPSE_TIME"|awk -F ':' '{print $2}'|tr -d ' '`
+        let "succNum=totalNum-failNum"
+   fi
 
-   failNum=`cat $resultSummaryInfoFile|grep 'fail:'|awk -F ':' '{print $2}'`
-   succNum=`cat $resultSummaryInfoFile|grep 'success:'|awk -F ':' '{print $2}'`
-   totalNum=`cat $resultSummaryInfoFile|grep 'total:'|awk -F ':' '{print $2}'`
-   elapseTime=`cat $resultSummaryInfoFile|grep 'totalTime:'|awk -F ':' '{print $2}'`
    echo ""
    echo "-----------------------"
    echo "Fail:$failNum"
@@ -99,6 +125,7 @@ function quit()
 }
 
 export -f run
+export -f run_cci
 export -f quit
 export -f help
 export -f print_summary
