91a248c93b9fbff01b6ec5c750e03e2ccf36fe54
Zaiqiang Fan <fanzq@navercorp.com>
2018-08-30 16:45:58 +0800

    [CUBRIDQA-790] fix to show core icon and refactor checking result (#524)
    * [CUBRIDQA-790] fix to show core icon and refactor checking result
    
    * [CUBRIDQA-790] show paths for core or fatal errors

--- CTP/isolation/src/com/navercorp/cubridqa/isolation/Test.java
+++ CTP/isolation/src/com/navercorp/cubridqa/isolation/Test.java
@@ -181,39 +181,66 @@ public class Test {
 		if (tc.startsWith("/") == false) {
 			tc = "$HOME/" + tc;
 		}
+
 		script.addCommand("sh runone.sh -r " + (context.getRetryTimes() + 1) + " " + tc + " " + context.getTestCaseTimeoutInSec() + " " + context.getTestingDatabase() + " 2>&1");
 		result = ssh.execute(script);
 		workerLog.println(result);
 
-		processCoreFile();
+		boolean passFlag = true;
+		if (result.indexOf("flag: NOK") != -1) {
+			passFlag = false;
+		}
 
-		String[] itemArrary = result.split("\n");
-		if (itemArrary == null) {
-			return false;
-		} else {
-			Properties props = new Properties();
-			String[] pair;
-			for (String item : itemArrary) {
-				pair = item.split(":");
-				if (pair.length == 2) {
-					props.put(pair[0].trim().toLowerCase(), pair[1].trim().toUpperCase());
-				}
-			}
-			String core = props.getProperty("core", "").trim();
-			if (core.equals("") == false) {
-				this.hasCore = true;
-				this.addResultItem("NOK", "FOUND CORE(S): " + core);
-			}
-
-			String flag = props.getProperty("flag", "").trim();
-			if (flag.equals("TIMEOUT")) {
-				this.addResultItem("NOK", "TIMEOUT");
-				isTimeOut = true;
-				return false;
-			} else {
-				return (!hasCore) && flag.equals("OK");
+		ArrayList<String> errors = extractItems(result, "found core file");
+		errors.addAll(extractItems(result, "found fatal error"));
+		if (errors.size() > 0) {
+			passFlag = false;
+			this.hasCore = true;
+			for (String e : errors) {
+				this.addResultItem("NOK", e);
 			}
 		}
+
+		if (passFlag == true) {
+			passFlag = result.indexOf("flag: OK") != -1;
+			if (passFlag == false) {
+				this.addResultItem("NOK", "Not found OK or NOK word.");
+			}
+		}
+		return passFlag;
+
+		// processCoreFile();
+	}
+	
+	private static ArrayList<String> extractItems(String source, String find) {
+		ArrayList<String> resultList = new ArrayList<String>();
+		if (source == null) {
+			return resultList;
+		}
+		int startPos = 0, endPos;
+		String item;
+		while (true) {
+			startPos = source.indexOf(find, startPos);
+			if (startPos == -1) {
+				break;
+			}
+			endPos = source.indexOf("\n", startPos);
+			if (endPos == -1) {
+				item = source.substring(startPos);
+			} else {
+				item = source.substring(startPos, endPos);
+				startPos = endPos;
+			}
+			item = item.trim();
+			if (resultList.contains(item) == false) {
+				resultList.add(item);
+			}
+
+			if (endPos == -1) {
+				break;
+			}
+		}
+		return resultList;
 	}
 
 	private void processCoreFile() throws Exception {
