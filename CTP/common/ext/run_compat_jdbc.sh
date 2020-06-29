1a3ac46dc35da6975e0ff8db10e424e9306effaa
Song won-ryong <38000047+swi0110@users.noreply.github.com>
2020-06-29 11:02:01 +0900

    [CUBRIDQA-1087] When sql ext test in JDBC compatibility, patch file was the wrong path. (#598)
    Refer to http://jira.cubrid.org/browse/CUBRIDQA-1087
    
    This shell file worked to same the test case repository and patch & exclude list repository.
    So, when executing the sql ext test, patch & exclude list path was 'cubrid-testcases-private' repository then patch was not applied.
    
    I was fixed it.
    
    
    Modified list
    
    - CTP/common/ext/run_compat_jdbc.sh

--- CTP/common/ext/run_compat_jdbc.sh
+++ CTP/common/ext/run_compat_jdbc.sh
@@ -82,7 +82,9 @@ function run_sql() {
         exit
     fi
     cp -f ${test_config_template} ${TEST_RUNTIME_CONF}
- 
+
+    compat_config_repo_name=cubrid-testcases 
+
     if [ "$COMPAT_BUILD_SCENARIOS" == "medium" ];then
         ctp_type="medium"
         git_repo_name=cubrid-testcases
@@ -111,8 +113,8 @@ function run_sql() {
         branch=$COMPAT_BUILD_SCENARIO_BRANCH_GIT
         if [ "$BUILD_IS_FROM_GIT" == "1" ];then
            exclude_branch=$BUILD_SCENARIO_BRANCH_GIT
-           exclude_file_dir=$HOME/${git_repo_name}/${ctp_scenario}/config/daily_regression_test_exclude_list_compatibility
-           run_git_update -f $HOME/${git_repo_name} -b $exclude_branch
+           exclude_file_dir=$HOME/${compat_config_repo_name}/${ctp_scenario}/config/daily_regression_test_exclude_list_compatibility
+           run_git_update -f $HOME/${compat_config_repo_name} -b $exclude_branch
         elif [ "$BUILD_IS_FROM_GIT" == "0" ];then
            exclude_branch=$BUILD_SVN_BRANCH_NEW
            exclude_file_dir=$HOME/dailyqa/$BUILD_SVN_BRANCH_NEW/config
@@ -120,8 +122,8 @@ function run_sql() {
         fi
     elif [ "${COMPAT_TEST_CATAGORY##*_}" == "D" ]; then
         branch=$BUILD_SCENARIO_BRANCH_GIT
-        exclude_file_dir=$HOME/${git_repo_name}/${ctp_scenario}/config/daily_regression_test_exclude_list_compatibility
-        run_git_update -f $HOME/${git_repo_name} -b $branch
+        exclude_file_dir=$HOME/${compat_config_repo_name}/${ctp_scenario}/config/daily_regression_test_exclude_list_compatibility
+        run_git_update -f $HOME/${compat_config_repo_name} -b $branch
     fi
     get_best_version_for_exclude_patch_file "${exclude_file_dir}" "$COMPAT_TEST_CATAGORY"
 
