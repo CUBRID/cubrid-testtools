92415da0b1b373c94e2a783cc53da97271ff4711
sjkim777 <sjkim@cubrid.com>
2021-06-18 03:42:02 +0900

    copyright

--- CTP/shell/src/com/navercorp/cubridqa/shell/deploy/DeploySCL.java
+++ CTP/shell/src/com/navercorp/cubridqa/shell/deploy/DeploySCL.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, CUBRID Corporation. All rights reserved.
+ * Copyright (c) 2016, Cubrid Corporation. All rights reserved.
 
  * 
  * Redistribution and use in source and binary forms, with or without 
