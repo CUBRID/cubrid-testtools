20a3a0c4f538418f543ba8200f1e8fc173e419fd
zionyun <seeon0001@naver.com>
2021-10-25 14:05:54 +0900

    add new java file for cbrd-24063

---
+++ CTP/sql/function/stored_procedure/src/SpTest8.java
@@ -0,0 +1,59 @@
+import java.sql.*;
+import java.util.*;
+public class SpTest8 {
+        public static String SP() throws Exception  {
+                String          URL = null;
+                Connection      conn = null;                
+                PreparedStatement       pstmt = null;
+                ResultSet       rs = null;
+                String          sql = null;                
+                try {
+                        Class.forName("cubrid.jdbc.driver.CUBRIDDriver");                        
+                        conn = DriverManager.getConnection("jdbc:default:connection:");
+                        pstmt = conn.prepareStatement("select class_name from db_class");
+                        rs = pstmt.executeQuery();
+                        if (rs.next()) {
+                                sql = rs.getString("class_name");
+                        }
+                        rs.close();
+                        pstmt.close();
+                        conn.close();                
+                } catch (Exception e) {
+                        e.printStackTrace();
+                } finally {
+                        if (rs != null) rs.close();
+                        if (pstmt != null) pstmt.close();
+                        if (conn != null) conn.close();                        
+                        return sql;
+                }
+        }
+
+	public static String SP2() throws Exception  {
+                String          URL = null;
+                Connection      conn = null;
+                PreparedStatement       pstmt = null;
+                ResultSet       rs = null;
+                String          sql = null;
+                try {
+                        Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
+                        conn = DriverManager.getConnection("jdbc:default:connection:");
+                        pstmt = conn.prepareStatement("select class_name from db_class");
+                        rs = pstmt.executeQuery();
+                        if (rs.next()) {
+                                sql = rs.getString("class_name");
+                        }
+                        rs.close();
+                        pstmt.close();
+                        conn.close();
+                } catch (Exception e) {
+                        e.printStackTrace();
+                } finally {
+                        if (rs != null) rs.close();
+                        if (pstmt != null) pstmt.close();
+                        if (conn != null) conn.close();
+			if (conn != null) conn.close();
+			if (conn != null) conn.close();
+                        return sql;
+                }
+        }
+}
