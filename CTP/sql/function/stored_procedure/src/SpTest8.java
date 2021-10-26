3a33dd0e4b12c04c953ba0066c0fbe5c6f4ba072
zionyun <seeon0001@naver.com>
2021-10-26 11:58:17 +0900

    fix SpTest8 to other SpTest code form

--- CTP/sql/function/stored_procedure/src/SpTest8.java
+++ CTP/sql/function/stored_procedure/src/SpTest8.java
@@ -1,55 +1,67 @@
 import java.sql.*;
 import java.util.*;
 public class SpTest8 {
-        public static String SP() throws Exception  {
-                String          URL = null;
-                Connection      conn = null;                
-                PreparedStatement       pstmt = null;
-                ResultSet       rs = null;
-                String          sql = null;                
-                try {
-                        Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
-                        conn = DriverManager.getConnection("jdbc:default:connection:");
-                        pstmt = conn.prepareStatement("select class_name from db_class");
-                        rs = pstmt.executeQuery();
-                        if (rs.next()) {
-                                sql = rs.getString("class_name");
-                        }
-                        rs.close();
-                        pstmt.close();
-                        conn.close();                
-                } finally {
-                        if (rs != null) rs.close();
-                        if (pstmt != null) pstmt.close();
-                        if (conn != null) conn.close();                        
-                        return sql;
-                }
-        }
+	public static String SP() {
+		String          URL = null;
+		Connection      conn = null;
+		PreparedStatement       pstmt = null;
+		ResultSet       rs = null;
+		String          sql = null;
+		try {
+			Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
+			conn = DriverManager.getConnection("jdbc:default:connection:");
+			pstmt = conn.prepareStatement("select class_name from db_class");
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				sql = rs.getString("class_name");
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			//e.printStackTrace();                
+		} finally {
+			try{
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+			return sql;
+		}
+	}
 
-	public static String SP2() throws Exception  {
-                String          URL = null;
-                Connection      conn = null;
-                PreparedStatement       pstmt = null;
-                ResultSet       rs = null;
-                String          sql = null;
-                try {
-                        Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
-                        conn = DriverManager.getConnection("jdbc:default:connection:");
-                        pstmt = conn.prepareStatement("select class_name from db_class");
-                        rs = pstmt.executeQuery();
-                        if (rs.next()) {
-                                sql = rs.getString("class_name");
-                        }
-                        rs.close();
-                        pstmt.close();
-                        conn.close();
-                } finally {
-                        if (rs != null) rs.close();
-                        if (pstmt != null) pstmt.close();
-                        if (conn != null) conn.close();
-			if (conn != null) conn.close();
-			if (conn != null) conn.close();
-                        return sql;
-                }
-        }
+	public static String SP2() {
+		String          URL = null;
+		Connection      conn = null;
+		PreparedStatement       pstmt = null;
+		ResultSet       rs = null;
+		String          sql = null;
+		try {
+			Class.forName("cubrid.jdbc.driver.CUBRIDDriver");
+			conn = DriverManager.getConnection("jdbc:default:connection:");
+			pstmt = conn.prepareStatement("select class_name from db_class");
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				sql = rs.getString("class_name");
+			}
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			//e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+				if (conn != null) conn.close();
+				if (conn != null) conn.close();
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+			return sql;
+		}
+	}
 }
