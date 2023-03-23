d43a2817f42b1395031ddc77e82b4ea33919efd5
zionyun <41980125+zionyun@users.noreply.github.com>
2023-03-23 11:14:44 +0900

    [CBRD-24693] Add java procedure for testing javasp of cbrd-24693 (#649)
    refer to http://jira.cubrid.org/browse/CBRD-24693
    refer to CUBRID/cubrid-testcases#1412

--- CTP/sql/function/stored_procedure/src/SpTest8.java
+++ CTP/sql/function/stored_procedure/src/SpTest8.java
@@ -64,4 +64,29 @@ public class SpTest8 {
 			return sql;
 		}
 	}
+
+	//This is a function to verify that a server-side JDBC request greater than 4096 is executed. (CBRD-24693)
+        public static int SP3(String table) throws Exception{
+                StringBuilder stringBuilder = new StringBuilder();
+		int ret = 0;
+                try{
+                        Connection conn = DriverManager.getConnection("jdbc:default:connection:");
+
+                        stringBuilder.append ("select * from ");
+                        for (int i = 0; i < 10000; i++) {
+                                stringBuilder.append (" ");
+                        }
+                        stringBuilder.append (table + ";");
+
+                        PreparedStatement pstmt = conn.prepareStatement(stringBuilder.toString());
+                        ResultSet rs = pstmt.executeQuery();
+                        rs.next();
+			ret = rs.getInt(1);
+                        pstmt.close();
+                        conn.close();
+                } catch (SQLException e) {
+                        System.err.println(e.getMessage());
+                }
+                return ret;
+        }
 }
