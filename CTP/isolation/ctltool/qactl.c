99a3fd209bfb04c91a545604fef6cedc79a4eebd
zionyun <seeon0001@naver.com>
2024-06-03 10:24:25 +0900

    Increase session test time due to degradation of debug version. (SHORT_DURATION_DEFAULT)

--- CTP/isolation/ctltool/qactl.c
+++ CTP/isolation/ctltool/qactl.c
@@ -88,7 +88,7 @@ extern int local_tm_isblocked (int tran_index);
 #define STRESSEXEC_TOKEN  STRESSEXEC_CMDNAME
 
 #define SLEEP_MSECS 10
-#define SHORT_DURATION_DEFAULT (30000)	/*  30 seconds */
+#define SHORT_DURATION_DEFAULT (50000)	/*  50 seconds */
 #define LONG_DURATION_DEFAULT (300000)	/* 300 seconds */
 
 #define VALID_CLIENT_ID(TBL, ID)  (((ID) > 0) && ((ID) <= (TBL)->slotsinuse))
