83735b1a1254133de25fa3c1fbfe9610795fe2f9
tw-kang <twkang@cubrid.com>
2025-04-22 17:24:03 +0900

    [CUBRIDQA-1257]modify error checking logic on run_cubrid_install (#706)
    http://jira.cubrid.org/browse/CUBRIDQA-1257

--- CTP/shell/src/com/navercorp/cubridqa/shell/deploy/DeployOneNode.java
+++ CTP/shell/src/com/navercorp/cubridqa/shell/deploy/DeployOneNode.java
@@ -118,7 +118,7 @@ public class DeployOneNode {
 		String result;
 		try {
 			result = ssh.execute(scripts);
-			if (!com.navercorp.cubridqa.common.CommonUtils.isEmpty(result) && (result.indexOf("ERROR") != -1 || result.indexOf("No such file") != -1)) {
+			if (!com.navercorp.cubridqa.common.CommonUtils.isEmpty(result) && (result.indexOf("[ERROR]") != -1 || result.indexOf("No such file") != -1)) {
 				isSucc = false;
 				log.println("[ERROR] build install fail!");
 			}
@@ -151,7 +151,7 @@ public class DeployOneNode {
 		String result;
 		try {
 			result = ssh.execute(scripts);
-			if (result != null && (result.indexOf("ERROR") != -1 || result.indexOf("No such file") != -1)) {
+			if (result != null && (result.indexOf("[ERROR]") != -1 || result.indexOf("No such file") != -1)) {
 				isSucc = false;
 				log.println("[ERROR] build install fail!");
 			}
