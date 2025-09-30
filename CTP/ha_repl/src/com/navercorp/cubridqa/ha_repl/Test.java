719aead0a2a8be9cfed60f618751e4843f641570
zionyun <seeon0001@naver.com>
2025-09-30 17:50:49 +0900

    add blank

--- CTP/ha_repl/src/com/navercorp/cubridqa/ha_repl/Test.java
+++ CTP/ha_repl/src/com/navercorp/cubridqa/ha_repl/Test.java
@@ -73,7 +73,7 @@ public class Test {
 		String buildId = context.getBuildId();
                 String[] versionParts = buildId.split("\\.");
 		
-		// Select common.inc by version:
+			// Select common.inc by version:
                 // <= 11.3 -> common.inc.legacy
                 // == 11.4 -> common.inc.114 (CUBRIDQA-1244)
                 // >= 11.5 -> common.inc (CBRD-25862)
